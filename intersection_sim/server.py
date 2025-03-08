0f442dfa9e54e35c04fa2ee2a6cd6713a6ceec33
Martin Rog <martinrog14@gmail.com>
2025-03-08 15:30:32 +0100

    vehicle shape change

--- intersection_sim/server.py
+++ intersection_sim/server.py
@@ -6,9 +6,18 @@ from agents import RoadCell, VehicleAgent, TrafficLightAgent
 def agent_portrayal(agent):
     if isinstance(agent, RoadCell):
         return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "gray", "Layer": 0}
+    
     elif isinstance(agent, VehicleAgent):
-        color = "red" if agent.speed > 0 else "blue" # Blue when stopped, red when moving
-        return {"Shape": "circle", "r": 0.5, "Filled": "true", "Color": color, "Layer": 1}
+            color = "red" if agent.speed > 0 else "blue"
+            return {
+                "Shape": "arrowHead",  
+                "scale": 0.8,  
+                "Color": color,
+                "Layer": 1,
+                "heading_x": agent.direction[0],
+                "heading_y": agent.direction[1]
+            }
+    
     elif isinstance(agent, TrafficLightAgent):
         color = "green" if agent.state == "Green" else "red"
         return {"Shape": "rect", "w": 0.5, "h": 0.5, "Filled": "true", "Color": color, "Layer": 2}
